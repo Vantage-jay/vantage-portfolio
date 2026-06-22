0a3241ef05be87488c26b89ccc08b8e660c2bd3b
Vantage jay <jesuferanmiojatunwase@gmail.com>
2026-06-22 23:31:56 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
           src="/images/profile.png"
           alt="Vantage"
           width={400}
-          height={400}
+          height={500}
           className="profile"
         />
       </div>
